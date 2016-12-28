212a8e10ff12866c060d85e87e357f9aa6b2cc9b
wuweijie <wu.weijie@outlook.com>
2016-12-29 00:20:24 +0800

    deploy

---
+++ deploy/deploy.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+echo "检测文件目录是否存在"
+if ! test -d ~/.workspace; then mkdir ~/.workspace; fi
+if ! test -d ~/.workspace/toys; then mkdir ~/.workspace/toys; fi
+if ! test -d ~/.workspace/toys/sources; then mkdir ~/.workspace/toys/sources; fi
+if ! test -d ~/.workspace/toys/packages; then mkdir ~/.workspace/toys/packages; fi
+cd ~/.workspace/toys/sources
+NEW_PROJECT_DIR=$(date +"%Y%m%d%H%M%S")
+if test -z $BRANCH; then
+  echo "branch不能为空"
+  exit 2;
+fi
+echo $NEW_PROJECT_DIR
+echo $BRANCH
+git clone git@git.qingniu.co:tianting/toys.git -b $BRANCH $NEW_PROJECT_DIR
+cd $NEW_PROJECT_DIR
+cp package.json ~/.workspace/toys/packages
+cd ~/.workspace/toys/packages
+npm i --registry=https://registry.npm.taobao.org
+cd -
+ln -s ~/.workspace/toys/packages/node_modules node_modules
+gulp
+cd ..
+if test -e current; then rm -rf current; fi
+if ! test -d /var/www; then mkdir /var/www; fi
+if test -e /var/www/toys; then rm -rf /var/www/toys; fi
+
+cp -rf ~/.workspace/toys/sources/$NEW_PROJECT_DIR /var/www/toys
+cd /var/www/toys
+cp -rf conf.d/* /etc/nginx/conf.d
+nginx -s reload
+if test $BRANCH = dev;
+then
+  npm run migrate
+  pm2 start pm2.dev.json
+else
+  npm run migrate_p
+  pm2 start pm2.json
+fi
+
+echo "删除时间过长的文件"
+cd ~/.workspace/toys/sources
+find ../ -maxdepth 1 -name "*[0-9]*" | sort | head -n -3 | xargs rm -rf
